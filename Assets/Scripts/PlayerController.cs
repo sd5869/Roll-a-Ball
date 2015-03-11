43b025a7c6d92a7969f051d3993a5cc72fcfb33f
Siddharth Dungarwal <sd5869@gmail.com>
2015-03-11 18:23:33 +0530

    Added Project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private int count;
+	public GUIText countText;
+	public GUIText winText;
+	void Start()
+	{
+		count = 0;
+		setCountText();
+		winText.text = "";
+	}
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement =new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().AddForce(movement*speed*Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive(false);
+			count+=1;
+			setCountText();
+		}
+
+	}
+	void setCountText()
+	{
+		countText.text = "Score:" + count.ToString ();
+		if (count == 16) {
+			winText.text="You Win";
+		}
+	}
+}
